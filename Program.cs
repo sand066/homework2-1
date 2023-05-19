e304ebb115cc68412e55af5f2ca9641c13bc2024
sand066 <sandnattaphon@gmail.com>
2023-05-20 06:12:04 +0700

    homework1

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    int num = int.Parse(Console.ReadLine());
+    while (num < 0)
+    {
+      Console.WriteLine("Invalid Pascal’s triangle row number.");
+      num = int.Parse(Console.ReadLine());
+    }
+    for (int i = 0; i <= num; i++)
+    {
+      for (int j = 0; j <= i; j++)
+      {
+        int a = Factorial(i) / (Factorial(j) * Factorial(i - j));
+        Console.Write("{0} ", a);
+      }
+      Console.WriteLine("");
+    }
+
+  }
+  static int Factorial(int num)
+  {
+    if (num <= 1)
+    {
+      return 1;
+    }
+    return num * Factorial(num - 1);
+  }
+
+}
